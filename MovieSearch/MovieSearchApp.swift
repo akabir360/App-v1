49d7390ea87826db707d5a1f2edffaf641c40baf
abukabir <360sayeed@gmail.com>
2023-05-01 05:58:50 +1000

    Initial Commit

---
+++ MovieSearch/MovieSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchApp.swift
+//  MovieSearch
+//
+//  Created by Ishrat Jahan Sadia on 1/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
